5e15ed960f197a8dfeabf06aea4a657cd6559421
KSVarlamov <varlamov.ks@mail.ru>
2023-01-13 12:02:05 +0400

    Решение задачи 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,9 +12,25 @@ public class Main {
         Runnable task = () -> {
             String route = generateRoute(ROUTE_STR, 100);
             int count = (int) route.chars().filter(c -> c == 'R').count();
-            System.out.printf("Количество поворотов направо: %s\n", count);
+            //System.out.printf("Количество поворотов направо: %s\n", count);
             updateFreq(count);
         };
+
+        Thread leaderPrinter = new Thread(() -> {
+            int i = 0;
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
+                        System.out.printf("[%04d] Текущий лидер: повторений %s (встретилось %s раз)\n", ++i, maxEntry.getKey(), maxEntry.getValue());
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        });
+        leaderPrinter.start();
 
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < THREAD_COUNT; i++) {
@@ -26,14 +42,14 @@ public class Main {
         for (Thread thread : threads) {
             thread.join();
         }
-
+        leaderPrinter.interrupt();
         printStatistic();
     }
 
     private static void printStatistic() {
-        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElse(null);
+        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
 
-        System.out.printf("Самое частое количество повторений %s (встретилось %s раз)\n", maxEntry.getKey(), maxEntry.getValue());
+        System.out.printf("\nСамое частое количество повторений %s (встретилось %s раз)\n", maxEntry.getKey(), maxEntry.getValue());
         System.out.println("Другие размеры:");
         sizeToFreq.entrySet().stream().filter(entry -> !entry.equals(maxEntry)).forEach(m -> System.out.printf("- %s (%s раз)\n", m.getKey(), m.getValue()));
     }
@@ -45,6 +61,7 @@ public class Main {
             } else {
                 sizeToFreq.put(count, 1);
             }
+            sizeToFreq.notify();
         }
     }
 
